89db1ae90efd26918461dc8f5991a55b42087ec3
hazoorpreet <143157794+hazoorpreet@users.noreply.github.com>
2026-08-01 16:53:09 +0800

    Add documentation for Map.

--- Map.h
+++ Map.h
@@ -2,28 +2,77 @@
 #define MAP_H
 
 #include <map>
+#include "Vector.h"
 
+/** @brief Minimal but complete Map class wrapper
+ *
+ * @author Hazoorpreet Singh
+ * @date 21/07/2026
+ */
 template <typename KT, typename VT>
 class Map {
 public:
+
+    /** @brief Create empty constructor
+     *
+     */
     Map();
 
+    /** @brief Create Map from another Map
+     *
+     * @param[in] other - Other map to copy
+     */
     Map(const Map & other);
 
+    /** @brief Copy Map from another Map
+     *
+     * @param[in] other - Other map to copy
+     */
     Map operator=(const Map & other);
 
+    /** @brief Read item from map by key
+     *
+     * @param[in] key - Key to retrieve item
+     */
     const VT & operator[](const KT & key) const;
 
+    /** @brief Modify item from map by key
+     *
+     * @param[in] key - Key to retrieve item
+     */
     VT & operator[](const KT & key);
 
+    /** @brief Check if key exists in map
+     *
+     * @param[in] key - Key to check for
+     * @retval True - Key found
+     * @retval False - Key not found
+     */
     bool Find(const KT & key) const;
 
+    /** @brief Emplace a new key into the map
+     *
+     * @param[in] key - Key to emplace value
+     * @param[in] Value to emplace
+     * @retval True - Value successfully emplaced at key
+     * @retval False - Value could not be emplaced at key as key already exists
+     */
     bool Emplace(const KT & key, const VT & value);
 
+    /** @brief Erase key/value from map
+     *
+     * @param[in] key - Key to erase
+     */
     void Erase(const KT & key);
 
+    /** @brief Clear map
+     *
+     */
     void Clear();
 
+    /** @brief Virtual destructor of map
+     *
+     */
     virtual ~Map();
 
 private:
